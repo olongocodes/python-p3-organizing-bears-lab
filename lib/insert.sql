836fc5d8dd38ab93b813ca41f00de656d26e1605
Kevin <kevinolongo9@gmail.com>
2023-12-05 08:43:59 +0300

    insert data

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "black", "optimistic", "Yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 21, "M", "White", "relaxed", "Yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 22, "F", "Violet", "peacefull", "No");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 23, "M", "blue", "social", "Yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 24, "F", "red", "short-tempered", "No");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 25, "M", "Yellow", "optimistic", "No");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 26, "F", "green", "social", "Yes");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , NULL, NUll, NULL, NULL, NULL);
+
+
